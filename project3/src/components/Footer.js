1b648540dc9172d35848850fc1f5cfb20232b910
jasenkozlatarevic <jasenko.zlatarevic@stu.ibu.edu.ba>
2024-06-10 01:30:44 +0200

    Project 3 - Portfolio

--- project3/src/components/Footer.js
+++ project3/src/components/Footer.js
@@ -3,8 +3,19 @@ import { Box, Typography } from '@mui/material';
 
 const Footer = () => {
   return (
-    <Box py={3} textAlign="center">
-      <Typography variant="body2" color="textSecondary">
+    <Box 
+      py={3} 
+      textAlign="center"
+      sx={{
+        backgroundColor: '#3f51b5',
+        color: '#fff',
+        position: 'fixed',
+        left: 0,
+        bottom: 0,
+        width: '100%',
+      }}
+    >
+      <Typography variant="body2">
         © {new Date().getFullYear()} Jasenko Zlatarević. All rights reserved.
       </Typography>
     </Box>
